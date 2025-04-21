37969a521c59171ae3b89de6e34e1b28cbc1f747
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2025-04-21 16:00:17 -0400

    Create practice.js

---
+++ practice.js
@@ -0,0 +1,19 @@
+// This temperature in Kelvins
+const kelvin = 0;
+
+// Finding degree in celsius
+let celsius;
+celsius= kelvin - 273;
+console.log(celsius);
+
+// Now in Fahrenhiet
+let fahrenhite;
+// Rounded the outup
+fahrenheit = Math.floor(celsius * (9/5) +32);
+console.log(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+
+let newton;
+newton = celsius * (33/100);
+console.log(newton);
