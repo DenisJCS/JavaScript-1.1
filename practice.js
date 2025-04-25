eb1900eb789ce53dc3742a94da0bf26bf4431a50
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2025-04-25 13:37:22 -0400

    Update practice.js

--- practice.js
+++ practice.js
@@ -30,3 +30,19 @@ if(sale) {
   console.log('Time to wait for a sale.')
 }
 
+//Ternary OPERATORS 
+let isLocked = false;
+
+
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+
+let favoritePhrase = 'Love That!';
+
+favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
+
